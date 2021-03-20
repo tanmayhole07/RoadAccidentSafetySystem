5b60744a6701d9eef1b87482ec3285830bbd032b
Tanmay Hole <tanmayhole99@gmail.com>
2021-03-20 17:38:55 +0530

    Add Emergency Contact to database

--- app/src/main/java/com/example/roadaccidentsafetysystem/Fragments/EmergencyContactsFragment.java
+++ app/src/main/java/com/example/roadaccidentsafetysystem/Fragments/EmergencyContactsFragment.java
@@ -1,21 +1,41 @@
 package com.example.roadaccidentsafetysystem.Fragments;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.roadaccidentsafetysystem.LoginActivity;
 import com.example.roadaccidentsafetysystem.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -65,9 +85,28 @@ public class EmergencyContactsFragment extends Fragment {
     }
 
     FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseUser user;
+
+    ProgressDialog pd;
+
     String mUID = "uid";
+    String name, number;
+
+    private final int REQUEST_CODE = 99;
 
     CardView policeCv, ambulanceCv, fireBrigadeCv, childHelplineCv;
+
+    RelativeLayout tabHelplineRv, tabContactsRv, helpLineBorderRv, contactsBorderRv, emergencyContactRv;
+    ScrollView helpLineNumberRv;
+    TextView helplinetext, contactstext;
+
+    Button addPersonBtn;
+
+
+    ArrayList<String> contactList = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -79,6 +118,18 @@ public class EmergencyContactsFragment extends Fragment {
         ambulanceCv = view.findViewById(R.id.ambulanceCv);
         fireBrigadeCv = view.findViewById(R.id.fireBrigadeCv);
         childHelplineCv = view.findViewById(R.id.childHelplineCv);
+
+        tabHelplineRv = view.findViewById(R.id.tabHelplineRv);
+        tabContactsRv = view.findViewById(R.id.tabContactsRv);
+        helpLineBorderRv = view.findViewById(R.id.helpLineBorderRv);
+        contactsBorderRv = view.findViewById(R.id.contactsBorderRv);
+        helplinetext = view.findViewById(R.id.helplinetext);
+        contactstext = view.findViewById(R.id.contactstext);
+
+        addPersonBtn = view.findViewById(R.id.addPersonBtn);
+
+        helpLineNumberRv = view.findViewById(R.id.helpLineNumberRv);
+        emergencyContactRv = view.findViewById(R.id.emergencyContactRv);
 
         policeCv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -120,11 +171,175 @@ public class EmergencyContactsFragment extends Fragment {
             }
         });
 
+        tabHelplineRv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHelplineUI();
+            }
+        });
+
+        tabContactsRv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showContactUI();
+            }
+        });
+
+        addPersonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                dialogAddPerson();
+            }
+        });
+
+        pd = new ProgressDialog(getActivity());
+        pd.setTitle("Please wait");
+        pd.setCanceledOnTouchOutside(false);
         firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        showHelplineUI();
+
+        databaseReference = firebaseDatabase.getReference("Users");
 
 
         return view;
     }
+
+    private void dialogAddPerson() {
+
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_add_contact, null);
+        final TextView nameContactTv = view.findViewById(R.id.nameContactTv);
+        final TextView addPersonTv = view.findViewById(R.id.addPersonTv);
+        Button addPersonAndContactBtn = view.findViewById(R.id.addPersonAndContactBtn);
+
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(getActivity());
+        builder.setView(view);
+
+        android.app.AlertDialog dialog = builder.create();
+        dialog.show();
+
+        addPersonTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK);
+                i.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                startActivityForResult(i, 101);
+            }
+        });
+
+        nameContactTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameContactTv.setText(name + " , " + number);
+            }
+        });
+
+
+
+
+        addPersonAndContactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dialogName, dialogNumber;
+                dialogName = name;
+                dialogNumber = number;
+
+                if (TextUtils.isEmpty(dialogName)) {
+                    Toast.makeText(getActivity(), "Please select a contact", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                dialog.dismiss();
+                addPerson(dialogName, dialogNumber);
+            }
+        });
+    }
+
+    private void addPerson(String dialogName, String dialogNumber) {
+        Toast.makeText(getActivity(), ""+dialogName + " " + dialogNumber, Toast.LENGTH_SHORT).show();
+
+        final String timeStamp = "" + System.currentTimeMillis();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("name", "" + dialogName);
+        hashMap.put("contact", "" + dialogNumber);
+        hashMap.put("timeStamp", "" + timeStamp);
+
+        databaseReference.child(user.getUid()).child("EmergencyContacts").child(timeStamp).setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        pd.dismiss();
+                        Toast.makeText(getActivity(), "Added Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==101 && resultCode == Activity.RESULT_OK){
+            Uri uri =  data.getData();
+            String cols[] = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER,
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+            Cursor rs = getActivity().getContentResolver().query(uri,cols,null,null,null);
+            if(rs.moveToFirst()){
+                number = (rs.getString(0));
+                name = (rs.getString(1));
+            }
+        }
+
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void showContactUI() {
+
+        helpLineBorderRv.setVisibility(View.INVISIBLE);
+        contactsBorderRv.setVisibility(View.VISIBLE);
+
+        emergencyContactRv.setVisibility(View.VISIBLE);
+        helpLineNumberRv.setVisibility(View.GONE);
+
+        helplinetext.setTextColor(getResources().getColor(R.color.white));
+        //tabOrderDetailsTv.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+        contactstext.setTextColor(getResources().getColor(R.color.primaryTextColor));
+        //tabOrderedProductsTv.setBackgroundResource(R.drawable.shape_rect04);
+
+    }
+
+    private void showHelplineUI() {
+
+        contactsBorderRv.setVisibility(View.INVISIBLE);
+        helpLineBorderRv.setVisibility(View.VISIBLE);
+
+        emergencyContactRv.setVisibility(View.GONE);
+        helpLineNumberRv.setVisibility(View.VISIBLE);
+
+        contactstext.setTextColor(getResources().getColor(R.color.white));
+        //tabOrderDetailsTv.setBackgroundResource(R.drawable.shape_rect04);
+
+        helplinetext.setTextColor(getResources().getColor(R.color.primaryTextColor));
+        //tabOrderedProductsTv.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private void callNow(String phoneNumber) {
 
